ff78ad57a2ac15b2695798774e5ca05ad7536b33
Masa <tokyoweb3@users.noreply.github.com>
2026-02-24 08:34:39 +0900

    AA

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,19 @@ import { logger } from './utils/logger';
 import { startBot } from './bot';
 import { acquireLock } from './utils/lockfile';
 
+const logoSuperLiquid = `
+      .           *                  .
+            /\\___/\\            z Z    
+    *      ( - . - )____________z          *  
+            \\_                __)                   
+              \\_  \\________/  /          .     
+                \\__)      \\__)
+
+     ~ Booting... LazyGravity ~
+`;
+
+console.log(logoSuperLiquid);
+
 // 二重起動防止: 既にBotが起動中なら即終了
 acquireLock();
 
